6a5bbf032cff45fdba616c8644915302d4f6c07b
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2023-02-22 01:34:11 +0100

    Update main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     // cargo run -- 0.0.0.0:6090
     let args: Vec<_> = std::env::args().skip(1).collect();
     log!("started server with args {args:?}");
-    if args.len() < 1 {
+    if args.is_empty() {
         let e = AError::new(AET::ValueError(format!("expected command line arg ip:port>, got {}", args.len())));
         e.log_exit();
     }
