d937d51dec4a4555a8fa7e98fb48d0265ecc59ce
Spydr06 <spydr06@web.de>
2023-03-17 09:07:26 +0100

    Begin of Vector2 and Matrix4 impl

---
+++ engine/src/util/vector.rs
@@ -0,0 +1,127 @@
+use std::ops::*;
+use std::f64;
+
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Vector2<T> {
+    pub x: T,
+    pub y: T
+}
+
+impl<T> Vector2<T> {
+    pub fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Into<f64> + Copy> Vector2<T> {
+    pub fn dist(&self, other: &Self) -> f64 {
+        let x = self.x.into() - other.x.into();
+        let y = self.y.into() - other.y.into();
+        f64::sqrt(x * x + y * y)
+    }
+}
+
+impl<T: Copy> Vector2<T> {
+    #[inline(always)]
+    pub fn x(&self) -> T {
+        self.x
+    }
+
+    #[inline(always)]
+    pub fn y(&self) -> T {
+        self.y
+    }
+}
+
+impl<T> From<(T, T)> for Vector2<T> {
+    fn from(value: (T, T)) -> Self {
+        Self {
+            x: value.0,
+            y: value.1
+        }
+    }
+}
+
+impl<T: Add<Output = T>> Add for Vector2<T> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y
+        }
+    }
+}
+
+impl<T: Sub<Output = T>> Sub for Vector2<T> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y
+        }
+    }
+}
+
+impl<T: Mul<Output = T>> Mul for Vector2<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y
+        }
+    }
+}
+
+impl<T: Div<Output = T>> Div for Vector2<T> {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y
+        }
+    }
+}
+
+impl<T: AddAssign> AddAssign for Vector2<T> {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+impl<T: SubAssign> SubAssign for Vector2<T> {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+    }
+}
+
+impl<T: MulAssign> MulAssign for Vector2<T> {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+    }
+}
+
+impl<T: DivAssign> DivAssign for Vector2<T> {
+    fn div_assign(&mut self, rhs: Self) {
+        self.x /= rhs.x;
+        self.y /= rhs.y;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_vector2() {
+        let output = Vector2::from((42, 69));
+        assert_eq!(Vector2::default() + output, output);
+        assert_eq!(Vector2::from((21, 34)) + Vector2::from((21, 35)), output);
+    }
+}
