ba544ada2a6ce43f17765ddaf347ee9ab413d494
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2023-03-19 16:11:34 +0100

    Update shader.rs

--- client/src/renderer/shader.rs
+++ client/src/renderer/shader.rs
@@ -160,7 +160,7 @@ impl Program {
             };
         }
         Ok((regions.remove("vertex").ok_or("did not find vertex region in shader".to_string()),
-            regions.remove("fragment").ok_or("did not find fragment region in shader".to_string())));
+            regions.remove("fragment").ok_or("did not find fragment region in shader".to_string())))
     }
 
     pub fn from_source(src: &str) -> Result<Self, String> {
