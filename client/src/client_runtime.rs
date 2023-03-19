7b87d85c5e1d217e465deeb0560271c23ee48a20
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2023-03-19 20:55:34 +0100

    Update client_runtime.rs

--- client/src/client_runtime.rs
+++ client/src/client_runtime.rs
@@ -238,7 +238,7 @@ impl ClientRuntime {
                 }).map_err(|e| {
                     e.log_exit();
                 }).unwrap();
-
+                std::thread::sleep(Duration::from_nanos(20));
                 let packets = self.nc.borrow_mut().queued_packets();
                 for packet in packets {
                     self.handle_packet(&packet)?;
