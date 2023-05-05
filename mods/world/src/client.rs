ff034cb2e7738b2fbbeaf3ffed826c9f3e1973cf
Spydr06 <spydr06@web.de>
2023-05-05 23:00:58 +0200

    first chunk render

--- mods/world/src/client.rs
+++ mods/world/src/client.rs
@@ -1,9 +1,9 @@
-use std::{collections::HashMap, rc::Rc, cell::RefCell};
+use std::{collections::HashMap, rc::Rc, cell::{RefCell, RefMut}};
 
-use aeonetica_client::{ClientMod, networking::messaging::{ClientHandle, ClientMessenger}, data_store::DataStore, renderer::{window::{OpenGlContextProvider, events::Event}, layer::Layer, context::Context, Renderer}, client_runtime::ClientHandleBox};
-use aeonetica_engine::{log, Id, util::{id_map::IdMap, type_to_id}, networking::messaging::ClientEntity, log_warn};
+use aeonetica_client::{ClientMod, networking::messaging::{ClientHandle, ClientMessenger}, data_store::DataStore, renderer::{window::{OpenGlContextProvider, events::Event}, layer::Layer, context::Context, Renderer, texture::{SpriteSheet, Texture}, Quad, TexturedQuad, SpriteQuad, shader}, client_runtime::ClientHandleBox};
+use aeonetica_engine::{log, Id, util::{id_map::IdMap, type_to_id, camera::Camera, vector::Vector2}, networking::messaging::ClientEntity, log_warn};
 
-use crate::common::Chunk;
+use crate::common::{Chunk, CHUNK_SIZE};
 
 pub struct WorldModClient {
 
@@ -19,29 +19,36 @@ impl ClientMod for WorldModClient {
         handlers.insert(type_to_id::<WorldHandle>(), || Box::new(WorldHandle::new()));
     }
 
-    fn start(&self, context: &mut Context, _store: &mut DataStore, gl_context_provider: &OpenGlContextProvider) {
+    fn start(&self, context: &mut Context, store: &mut DataStore, gl_context_provider: &OpenGlContextProvider) {
         gl_context_provider.make_context();
 
-        context.push(Rc::new(WorldLayer::instantiate()))
+        context.push(Rc::new(WorldLayer::instantiate()));
     }
 }
 
 pub(crate) struct WorldHandle {
-    chunk: Option<Chunk>,
-    new: bool
+    chunks: Vec<Chunk>,
+    chunk_queue: Vec<Chunk>,
+    tile_sprites: SpriteSheet,
+    shader: Rc<shader::Program>,
 }
 
 impl WorldHandle {
     fn new() -> Self {
         Self {
-            chunk: None,
-            new: false
+            chunks: vec![],
+            chunk_queue: vec![],
+            tile_sprites: SpriteSheet::from_texture(
+                Texture::from_bytes(include_bytes!("../assets/include/stone.png")).unwrap(),
+                Vector2::new(16, 16)
+            ).expect("error loading world spritesheet"),
+            shader: Rc::new(shader::Program::from_source(include_str!("../assets/shaders/world.glsl")).unwrap())
         }
     }
 
     pub(crate) fn receive_chunk_data(&mut self, data: Chunk) {
         log_warn!("receive_chunk_data() called");
-        self.chunk = Some(data);
+        self.chunk_queue.push(data);
     }
 }
 
@@ -54,25 +61,38 @@ impl ClientHandle for WorldHandle {
         messenger.register_receiver(Self::receive_chunk_data);
     }
 
-    fn update(&mut self, messenger: &mut ClientMessenger, renderer: &std::cell::RefMut<Renderer>, delta_time: f64) {
-        if self.new {
-            self.new = false;
-        
-            log!("new chunk detected");
-        }
+    fn update(&mut self, messenger: &mut ClientMessenger, renderer: &mut RefMut<Renderer>, delta_time: f64) {
+        self.chunk_queue.drain(..).for_each(|chunk| {
+            for (i, _) in chunk.tiles().iter().enumerate() {
+                let x = (i % CHUNK_SIZE * 16) as i32 + chunk.chunk_pos.x() * CHUNK_SIZE as i32;
+                let y = (i / CHUNK_SIZE * 16) as i32 + chunk.chunk_pos.y() * CHUNK_SIZE as i32;
+                let mut quad = SpriteQuad::new(
+                    Vector2::new(x as f32, y as f32), 
+                    Vector2::new(16.0, 16.0), 
+                    0, 
+                    self.tile_sprites.get(0).unwrap(), 
+                    self.shader.clone()
+                );
+                renderer.add(&mut quad);
+            }
 
-        log!("update...")
+            log!("new chunk detected {:?}", chunk);
+            
+            self.chunks.push(chunk);
+        });
     }
 }
 
 pub struct WorldLayer {
-    renderer: RefCell<Renderer>
+    renderer: RefCell<Renderer>,
+    camera: RefCell<Camera>,
 }
 
 impl Layer for WorldLayer {
     fn instantiate() -> Self where Self: Sized {
         Self {
-            renderer: RefCell::new(Renderer::new())
+            renderer: RefCell::new(Renderer::new()),
+            camera: RefCell::new(Camera::new(0.0, 1280.0, 720.0, 0.0, -1.0, 1.0))
         }
     }
 
@@ -86,9 +106,10 @@ impl Layer for WorldLayer {
 
     fn on_update(&self, handles: &mut IdMap<ClientHandleBox>, delta_time: f64) {
         let mut renderer = self.renderer.borrow_mut();
-        handles.iter_mut().for_each(|(_, h)| h.update(&renderer, delta_time));
-
+        renderer.begin_scene(&*self.camera.borrow());
+        handles.iter_mut().for_each(|(_, h)| h.update(&mut renderer, delta_time));
         renderer.draw_vertices();
+        renderer.end_scene();
     }
 
     fn on_event(&self, _handles: &mut IdMap<ClientHandleBox>, _event: &Event) -> bool {
--- mods/world/src/common.rs
+++ mods/world/src/common.rs
@@ -6,7 +6,7 @@ pub type Tile = u16;
 
 pub const CHUNK_SIZE: usize = 16;
 
-#[derive(SerBin, DeBin)]
+#[derive(SerBin, DeBin, Debug, Clone)]
 pub struct Chunk {
     pub chunk_pos: Vector2<u32>,
     pub tiles: [Tile; CHUNK_SIZE*CHUNK_SIZE]
@@ -18,5 +18,9 @@ impl Chunk {
             chunk_pos,
             tiles: [0; CHUNK_SIZE*CHUNK_SIZE]
         }
+    }
+
+    pub(crate) fn tiles(&self) -> &[Tile; CHUNK_SIZE * CHUNK_SIZE] {
+        &self.tiles
     }
 }
