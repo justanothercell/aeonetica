0491d88462cccfc1f9c80a64ef2e42fb3e31c82c
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2023-05-05 23:15:14 +0200

    tiles to 1

--- mods/world/src/common.rs
+++ mods/world/src/common.rs
@@ -14,10 +14,17 @@ pub struct Chunk {
 
 impl Chunk {
     pub(crate) fn new(chunk_pos: Vector2<i32>) -> Self {
-        Self {
+        let mut s = Self {
             chunk_pos,
             tiles: [0; CHUNK_SIZE*CHUNK_SIZE]
+        };
+        for i in 0..CHUNK_SIZE {
+            if i != 7 && i != 8 {
+                *s.mut_tile((0, i as i32).into()) = 1;
+                *s.mut_tile((i as i32, 0).into()) = 1;
+            }
         }
+        s
     }
 
     pub(crate) fn tiles(&self) -> &[Tile; CHUNK_SIZE * CHUNK_SIZE] {
