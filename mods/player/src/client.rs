fa89404cd91b69f0c349a2c756af051ff6cb147a
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2023-05-05 23:40:14 +0200

    missing imports

--- mods/player/src/client.rs
+++ mods/player/src/client.rs
@@ -1,3 +1,5 @@
+use aeonetica_client::ClientMod;
+
 pub struct PlayerModClient {
 
 }
--- mods/player/src/server.rs
+++ mods/player/src/server.rs
@@ -1,7 +1,9 @@
+use aeonetica_server::ServerMod;
+
 pub struct PlayerModServer {
 
 }
 
-impl PlayerModServer {
+impl ServerMod for PlayerModServer {
 
 }
