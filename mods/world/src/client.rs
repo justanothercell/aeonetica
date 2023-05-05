aaaf5b722066a6a027832798ad68d49563e69a0a
Spydr06 <spydr06@web.de>
2023-05-05 20:47:36 +0200

    implement ClientMod for world

--- mods/world/src/client.rs
+++ mods/world/src/client.rs
@@ -1,3 +1,21 @@
-pub struct TestModClient {
+use std::collections::HashMap;
 
+use aeonetica_client::{ClientMod, networking::messaging::ClientHandle, data_store::DataStore, renderer::window::OpenGlContextProvider};
+use aeonetica_engine::{log, util::type_to_id, Id};
+
+pub struct WorldModClient {
+
+}
+
+impl ClientMod for WorldModClient {
+    fn init(&mut self, _flags: &Vec<String>) {
+        log!("hello from client testmod!");
+    }
+
+    fn register_handlers(&self, handlers: &mut HashMap<Id, fn() -> Box<dyn ClientHandle>>) {
+        log!("handles registered");
+    }
+
+    fn start(&self, context: &mut aeonetica_client::renderer::context::Context, _store: &mut DataStore, gl_context_provider: &OpenGlContextProvider) {
+    }
 }
--- server/recompile-mods.sh
+++ server/recompile-mods.sh
@@ -2,7 +2,7 @@
 set -xe
 
 pushd ../mods
-python3 build.py -w testmod -d ../server/mods
+python3 build.py -w world -d ../server/mods
 popd
 
 if [[ $1 == '-r' ]] || [[ $1 == '--run' ]]; then
