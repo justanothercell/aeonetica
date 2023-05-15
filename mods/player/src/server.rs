c045eae7fc8f2cc9988ca7fa5f5915a8f206cd83
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2023-05-15 21:00:12 +0200

    Update server.rs

--- mods/player/src/server.rs
+++ mods/player/src/server.rs
@@ -97,7 +97,7 @@ impl Player {
     pub(crate) fn client_position_update(id: &EntityId, engine: &mut Engine, _client_id: &ClientId, (position, teleporting): (Vector2<f32>, bool)) {
         let player = &mut *engine.mut_entity(id);
         player.mut_module::<Player>().unwrap().position = position;
-        player.mut_module::<Messenger>().unwrap().call_client_fn(PlayerHandle::receive_position, (position, teleporting), SendMode::Safe);
+        player.mut_module::<Messenger>().unwrap().call_client_fn(PlayerHandle::receive_position, (position, teleporting), SendMode::Safe );
     }
 }
 
